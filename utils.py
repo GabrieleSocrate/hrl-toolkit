366b85c10e610950a3e6acec6110c079585518d0
Gabriele Socrate <gabrielesocrate@icloud.com>
2026-01-17 18:32:04 +0100

    Modified Utils

--- utils.py
+++ utils.py
@@ -1,54 +1,27 @@
 import gymnasium as gym
 import numpy as np
 import torch
+import random
 
-from gymnasium.wrappers import AtariPreprocessing, TransformReward
-from gymnasium.wrappers import FrameStackObservation as FrameStack_
+def set_seed(seed):
+    """In Python there isn't a single "global" random  generator.
+    random, NumPy, PyTorch and the enviroment all use their own randomness.
+    We set the same seed everywhere so the code behaves the same every time we run it"""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
-from fourrooms import Fourrooms
-
-
-class LazyFrames(object):
-    def __init__(self, frames):
-        self._frames = frames
-
-    def __array__(self, dtype=None):
-        out = np.stack(self._frames, axis=0)
-        if dtype is not None:
-            out = out.astype(dtype)
-        return out
-
-    def __len__(self):
-        return len(self.__array__())
-
-    def __getitem__(self, i):
-        return self.__array__()[i]
-
-
-class FrameStack(FrameStack_):
-    def __init__(self, env, k):
-        FrameStack_.__init__(self, env, k)
-
-    def _get_ob(self):
-        assert len(self.frames) == self.k
-        return LazyFrames(list(self.frames))
-
-def make_env(env_name):
-
-    if env_name == 'fourrooms':
-        return Fourrooms(), False
-    if env_name.startswith("ALE/"):
-        import ale_py 
+def make_env(env_name, seed = 0):
     env = gym.make(env_name)
-    is_atari = env_name.startswith("ALE/")
+    env.reset(seed = seed)
+    env.action_space.seed(seed)
+    return env
 
-    if is_atari:
-        env = AtariPreprocessing(env, grayscale_obs=True, scale_obs=True, terminal_on_life_loss=True)
-        env = TransformReward(env, lambda r: np.clip(r, -1, 1))
-        env = FrameStack(env, 4)
-    return env, is_atari
-
-def to_tensor(obs):
-    obs = np.asarray(obs)
-    obs = torch.from_numpy(obs).float()
-    return obs
+def to_tensor(obs, device = None):
+    obs = np.asarray(obs, dtype = np.float32)
+    t = torch.as_tensor(obs, dtype = torch.float32)
+    if device is not None:
+        t = t.to(device)
+    return t
